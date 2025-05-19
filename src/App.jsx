0c188fdeea07ec456d9cb947908f2a576873c79e
houssam1121 <houssammasri102@gmail.com>
2025-05-20 01:33:19 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import HeroSection from './components/HeroSection';
+
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
